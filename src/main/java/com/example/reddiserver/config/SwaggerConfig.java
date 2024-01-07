9f79c83551729d5ba269d36c7fadf5fbe3bb625c
younggyo <lee4519698@hanmail.net>
2024-01-07 20:45:59 +0900

    chore : swagger 연결

---
+++ src/main/java/com/example/reddiserver/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.example.reddiserver.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    private static final String SECURITY_SCHEME_NAME = "authorization";	// 추가
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                    .title("API Document")
+                    .version("v0.0.1")
+                    .description("REDDI API 명세서");
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                                .name(SECURITY_SCHEME_NAME)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .info(info);
+    }
+}
