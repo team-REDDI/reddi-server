e9008862b9d93afa0e9cfda92cebbece8a2f8660
younggyo <lee4519698@hanmail.net>
2024-01-07 18:14:23 +0900

    setting : 초기 환경 설정

---
+++ src/main/java/com/example/reddiserver/ReddiServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.reddiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReddiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReddiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reddiserver/ReddiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reddiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReddiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
