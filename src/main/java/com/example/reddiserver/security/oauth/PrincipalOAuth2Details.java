a73a56e8bd358297163fc095a6ad0463ea16478a
younggyo <lee4519698@hanmail.net>
2024-01-10 18:49:11 +0900

    feat : create PrincipalOAuth2Details, Service

---
+++ src/main/java/com/example/reddiserver/security/oauth/PrincipalOAuth2Details.java
@@ -0,0 +1,49 @@
+package com.example.reddiserver.security.oauth;
+
+import com.example.reddiserver.entity.Member;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+@Getter
+public class PrincipalOAuth2Details implements OAuth2User {
+
+    private Member member;
+    private Map<String, Object> attributes;
+
+    public PrincipalOAuth2Details(Member member) {
+        this.member = member;
+    }
+
+    public PrincipalOAuth2Details(Member member, Map<String, Object> attributes) {
+        this.member = member;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collect = new ArrayList<>();
+        collect.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return member.getAuthority().toString();
+            }
+        });
+
+        return collect;
+    }
+
+    @Override
+    public String getName() {
+        return "name";
+    }
+}
