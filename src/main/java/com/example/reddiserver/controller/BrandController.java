8009d423cfb79165c32fed67f27b390e94de5c39
itsme-shawn <chs98105@gmail.com>
2024-01-20 22:10:03 +0900

    브랜드 조회 api

--- src/main/java/com/example/reddiserver/controller/BrandController.java
+++ src/main/java/com/example/reddiserver/controller/BrandController.java
@@ -1,7 +1,10 @@
 package com.example.reddiserver.controller;
 
 import com.example.reddiserver.common.ApiResponse;
+import com.example.reddiserver.dto.brand.response.BrandContentsResponseDto;
+import com.example.reddiserver.dto.brand.response.BrandResponseDto;
 import com.example.reddiserver.repository.BrandRepository;
+import com.example.reddiserver.service.BrandService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +18,23 @@ import java.util.List;
 @RequestMapping("/api/brand")
 public class BrandController {
 
-	private final BrandRepository brandRepository;
+	private final BrandService brandService;;
+
+	@Operation(summary = "브랜드 리스트 조회")
+	@GetMapping("/")
+	public ApiResponse<List<BrandResponseDto>> getBrandList() {
+		List<BrandResponseDto> brandList = brandService.getBrandList();
+		return ApiResponse.successResponse(brandList);
+	}
+
+	@Operation(summary = "브랜드 단건(상세) 조회")
+	@GetMapping("/{id}")
+	public ApiResponse<BrandContentsResponseDto> getBrandById(Long id) {
+		BrandContentsResponseDto brand = brandService.getBrandById(id);
+		return ApiResponse.successResponse(brand);
+	}
+
+
 
 
 
---
+++ src/main/java/com/example/reddiserver/dto/brand/response/BrandTagDto.java
@@ -0,0 +1,31 @@
+package com.example.reddiserver.dto.brand.response;
+
+import com.example.reddiserver.entity.BrandTag;
+import com.example.reddiserver.entity.enums.BrandTagType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class BrandTagDto {
+	private BrandTagType brandTagType;
+	private String tag;
+
+	public BrandTagDto(BrandTag brandTag) {
+		this.brandTagType = brandTag.getBrandTagType();
+		this.tag = brandTag.getTag();
+	}
+
+	// Getters and setters (if needed)
+
+	public static List<BrandTagDto> convertToDtoList(List<BrandTag> brandTags) {
+		return brandTags.stream()
+				.map(BrandTagDto::new)
+				.collect(Collectors.toList());
+	}
+}
