ebd13a4e7e39121b0d614e2a616da595e1ea549a
younggyo <lee4519698@hanmail.net>
2024-01-27 16:22:23 +0900

    feat : update brand, postRepository

--- src/main/java/com/example/reddiserver/repository/BrandRepository.java
+++ src/main/java/com/example/reddiserver/repository/BrandRepository.java
@@ -1,6 +1,7 @@
 package com.example.reddiserver.repository;
 
 import com.example.reddiserver.entity.Brand;
+import com.example.reddiserver.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -19,4 +20,7 @@ public interface BrandRepository extends JpaRepository<Brand, Long> {
 
 	@Query("SELECT b FROM Brand b ORDER BY b.view_count DESC, b.name ASC")
 	List<Brand> findTopNByOrderByViewCountDescAndNameAsc(Pageable pageable);
+
+	@Query("SELECT b FROM Brand b WHERE b.name like :keyword OR b.content like :keyword")
+	Page<Brand> findByNameContainingOrContentContaining(String keyword, Pageable pageable);
 }
