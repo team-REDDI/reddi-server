70cbc068d0c6bcea6e0165b1680361a8026121cc
younggyo <lee4519698@hanmail.net>
2024-01-30 01:45:01 +0900

    fix : update 브랜딩 AI dto

--- src/main/java/com/example/reddiserver/dto/chatgpt/response/ChatGptResponse.java
+++ src/main/java/com/example/reddiserver/dto/chatgpt/response/ChatGptResponse.java
@@ -1,8 +1,29 @@
 package com.example.reddiserver.dto.chatgpt.response;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Builder
+@Getter
+@NoArgsConstructor
 public class ChatGptResponse {
-    private String result;
+    private String name;
+    private String reason;
+    private String slogan;
+    private String vision;
+    private String essence;
+    private String keyword;
+    private String manifesto;
+
+    @Builder
+    public ChatGptResponse(String name, String reason, String slogan, String vision,
+                           String essence, String keyword, String manifesto) {
+        this.name = name;
+        this.reason = reason;
+        this.slogan = slogan;
+        this.vision = vision;
+        this.essence = essence;
+        this.keyword = keyword;
+        this.manifesto = manifesto;
+    }
 }
