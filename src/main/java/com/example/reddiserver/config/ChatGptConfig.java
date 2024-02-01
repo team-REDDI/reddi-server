ab9e0ffd8e9890f3e6f4cf15b9732749b8b0def4
itsme-shawn <chs98105@gmail.com>
2024-02-02 08:44:39 +0900

    feat: gpt 모델 업데이트 및 프롬프트 수정

--- src/main/java/com/example/reddiserver/config/ChatGptConfig.java
+++ src/main/java/com/example/reddiserver/config/ChatGptConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ChatGptConfig {
-    public static final String CHAT_MODEL = "gpt-3.5-turbo";
+    public static final String CHAT_MODEL = "gpt-3.5-turbo-0125";
     public static final String SYSTEM_ROLE = "system";
     public static final String USER_ROLE = "user";
     public static final String CHAT_URL = "https://api.openai.com/v1/chat/completions";
